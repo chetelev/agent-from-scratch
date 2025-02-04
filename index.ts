9b2db00c6cd93376fcbba0b4903a06c4a0b00c8c
Dimitar Chetelev <c_dchetelev@capsuletech.com>
2025-02-04 13:30:37 +0100

    save prompt to json db, in memory chat integration

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
 import 'dotenv/config'
+import { runLLM } from './src/llm'
+import { addMessages } from './src/memory'
+import { getMessages } from './src/memory'
 
-const userMessage = process.argv[2]
-
+const userMessage = process.argv.slice(2).join(' ')
 if (!userMessage) {
   console.error('Please provide a message')
   process.exit(1)
 }
+
+await addMessages([{ role: 'user', content: userMessage }])
+const messages = await getMessages()
+
+const response = await runLLM({
+  messages,
+})
+
+await addMessages([{ role: 'assistant', content: response.content }])
